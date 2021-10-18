e4fee8f721ec99829dde6c54e7ed431a18beba87
Natalia Orsi <naty.m.orsi@gmail.com>
2021-10-18 09:14:25 +0200

    Added the date to the forecast

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -31,10 +31,47 @@ export default function WeatherForecastDay(props) {
        return days[day];
      }
 
+     function month() {
+       let date = new Date(props.data.dt * 1000);
+       let month = date.getMonth();
+
+       let months = [
+         "January",
+         "February",
+         "March",
+         "April",
+         "May",
+         "June",
+         "July",
+         "August",
+         "September",
+         "October",
+         "November",
+         "December"
+       ]
+       return months[month];
+     }
+
+     function daysOfMonth() {
+       let date = new Date(props.data.dt * 1000);
+       let day = date.getDate();
+
+       if (day === 1 || day === 21 || day === 31) {
+         return `${day}st`
+       } else if (day === 2 || day === 22) {
+         return `${day}nd`;
+       } else if (day === 3 || day === 23) {
+         return `${day}rd`;
+       } else {
+         return `${day}th`;
+       }
+
+     }
+
     return (
       <div>
         <div className="forecast-week-day">{day()}</div>
-        <div className="forecast-week-date">October, 15th</div>
+        <div className="forecast-week-date">{month()}, {daysOfMonth()}</div>
         <div className="weather-icons">
           <WeatherIcon code={props.data.weather[0].icon} size={48} />
         </div>
