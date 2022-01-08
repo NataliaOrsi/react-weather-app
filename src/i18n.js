4aa0335688c91620eb09e281c28d97d14a057c47
Natalia Orsi <naty.m.orsi@gmail.com>
2022-01-08 13:48:27 +0100

    Added json archives for the translation

--- src/i18n.js
+++ src/i18n.js
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
+import Backend from "i18next-http-backend";
 
 i18n
   // detect user language
@@ -10,33 +11,18 @@ i18n
   .use(initReactI18next)
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
+  .use(Backend)
   .init({
-    debug: true,
-    fallbackLng: "en",
-    interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+    react: {
+      // By default useSuspense is set to true so React needs fallback UI. 
+      // Setting useSuspense to false will solve your problem as React will not need fallback UI anymore.
+      useSuspense: false,
     },
-    resources: {
-      en: {
-        translation: {
-          description: {
-            part1: 'Humidity: ',
-            part2: 'Wind: ',
-            searchButton: 'Search',
-            suggestion: "Enter a city..."
-          }
-        },
-      },
-      pt: {
-        translation: {
-          description: {
-            part1: 'Umidade: ',
-            part2: 'Vento: ',
-            searchButton: "Procurar",
-            suggestion: 'Indique uma cidade...'
-          }
-        }
-      }
+    // reference: https://www.i18next.com/how-to/add-or-load-translations#load-using-a-backend-plugin
+    backend: {
+      fallbackLng: "en",
+      supportedLngs: ["en", "pt"],
+      loadPath: "/assets/locales/{{lng}}/translation.json",
     },
   });
 
